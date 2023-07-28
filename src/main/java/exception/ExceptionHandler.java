05346242568f5ae4f6266e0a4004a0d3585ac827
Tzeentche <ilyasugako87@gmail.com>
2023-07-28 23:44:43 +0200

    Add exceptions handling

---
+++ src/main/java/exception/ExceptionHandler.java
@@ -0,0 +1,14 @@
+package exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public ResponseEntity<ValidationErrorResponse> handleValidationException(ValidationException e) {
+        return new ResponseEntity(new ValidationErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
+
